820e10d3646c387c727b72066db25eca7f9c8e31
Alfio Martini <alfio.martini@gmail.com>
2021-11-20 10:17:40 -0300

    Changed css styling of the calculator

--- src/static/index.js
+++ src/static/index.js
@@ -7,19 +7,27 @@ const calc = new Calculator();
 
 const header = document.querySelector(".header");
 const keys = document.querySelector(".keys");
-const constants = keys.querySelector(".constants");
-const operators = keys.querySelector(".operators");
 
-const clear = header.querySelector(".clear");
+const clear = header.querySelector("#clear");
 const input = header.querySelector("input");
-input.value = "";
+clearInput();
 
-constants.addEventListener("click", processKeyboard);
-operators.addEventListener("click", processKeyboard);
+keys.addEventListener("click", processKeyboard);
 input.addEventListener("input", processInput);
-clear.addEventListener("click", function () {
+clear.addEventListener("click", clearInput);
+
+let state = {
+  memory: 0,
+};
+
+function setState(payload) {
+  state = { ...state, ...payload };
+}
+
+function clearInput() {
   input.value = "";
-});
+  input.focus();
+}
 
 function processInput(event) {
   let { value } = event.target;
@@ -32,18 +40,34 @@ function processInput(event) {
 }
 
 function processKeyboard(event) {
-  const { innerText } = event.target;
-  if (innerText === "=") {
+  console.log(event.target);
+  const { id, innerText } = event.target;
+  if (id === "equals") {
     const value = input.value;
     computeValue(value);
-  } else {
-    let text = input.value;
-    input.value = text + innerText;
+    return;
   }
+  if (id === "clear") {
+    clearInput();
+    return;
+  }
+  if (id === "bsp") {
+    let value = input.value;
+    if (value.length > 0) {
+      input.value = value.slice(0, value.length - 1);
+      input.focus();
+    }
+    input.focus();
+    return;
+  }
+  let text = input.value;
+  input.value = text + innerText;
+  input.focus();
 }
 
 function computeValue(val) {
   let result = calc.eval(val);
   console.log("result", result);
   input.value = calc.display;
+  input.focus();
 }
