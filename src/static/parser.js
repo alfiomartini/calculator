86a9317ceb8065b4ab65444dbb10d0c75ddfd393
Alfio Martini <alfio.martini@gmail.com>
2021-11-19 23:12:22 -0300

    Removed unecessary 'elses' from parseT function

--- src/static/parser.js
+++ src/static/parser.js
@@ -10,7 +10,6 @@ function parseE(exprStr) {
   exprStr = skipSpaces(exprStr);
   let match = /^[+-]?((\d+(\.\d*)?)|\.\d+)/.exec(exprStr);
   if (match) {
-    // console.log("match", match);
     let exprTree = { type: "number", value: match[0] };
     exprStr = exprStr.slice(match[0].length);
     return parseT(exprTree, exprStr);
@@ -33,28 +32,26 @@ function parseT(exprTree, exprStr) {
   exprStr = skipSpaces(exprStr);
   if (exprStr === "") {
     return { exprTree, exprStr };
-  } else {
-    let match = /^[-+/*/]/.exec(exprStr);
-    if (!match) {
-      // console.log("Operator expected.");
-      return { exprTree, exprStr };
-    } else {
-      // push arg (left child)
-      const arg01 = exprTree;
-      // console.log("arg01, restExpr", arg01, exprStr);
-      const semTree = { type: "apply", operator: match[0], args: [] };
-      semTree.args.push(arg01);
-      // remove op
-      exprStr = exprStr.slice(1);
-      let result = parseE(exprStr);
-      // push arg (right child)
-      const arg02 = result.exprTree;
-      semTree.args.push(arg02);
-      // console.log("arg02, restExpr", arg02, result.exprStr);
-
-      return { exprTree: semTree, exprStr: result.exprStr };
-    }
   }
+  let match = /^[-+/*/]/.exec(exprStr);
+  if (!match) {
+    // console.log("Operator expected.");
+    return { exprTree, exprStr };
+  }
+  // push arg (left child)
+  const arg01 = exprTree;
+  // console.log("arg01, restExpr", arg01, exprStr);
+  const semTree = { type: "apply", operator: match[0], args: [] };
+  semTree.args.push(arg01);
+  // remove op
+  exprStr = exprStr.slice(1);
+  let result = parseE(exprStr);
+  // push arg (right child)
+  const arg02 = result.exprTree;
+  semTree.args.push(arg02);
+  // console.log("arg02, restExpr", arg02, result.exprStr);
+
+  return { exprTree: semTree, exprStr: result.exprStr };
 }
 
 function skipSpaces(text) {
